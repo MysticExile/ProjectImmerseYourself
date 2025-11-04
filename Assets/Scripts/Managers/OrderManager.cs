1c060a0a007869460724d971e79a780fcd67194d
Jens Ouwens <jensouwe@xs4all.nl>
2025-11-04 12:22:06 +0100

    Finished: OrderManager

--- Assets/Scripts/Managers/OrderManager.cs
+++ Assets/Scripts/Managers/OrderManager.cs
@@ -1,17 +1,27 @@
 using System;
 using System.Collections.Generic;
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
+[Serializable]
+public struct Orders
+{
+    public string orderName;
+    public Sprite orderImage;
+}
 public class OrderManager : MonoBehaviour
 {
     [SerializeField] private TimerManager timerManager;
-    [SerializeField] private GameObject OrderPlaceHolder;
+    [SerializeField] private TMP_Text OrderPlaceHolder;
+    [SerializeField] private GameObject SpaceToConfirm;
     [SerializeField] private Image OrderImage;
-    [SerializeField] private Dictionary<int, Image> orderHolder;
-    private int currentOrder;
+    [SerializeField] private List<Orders> orderHolder;
+    private int currentOrderIndex;
+    private Orders currentOrder;
     private int orderConformations;
     void Update()
     {
@@ -26,7 +36,7 @@ public class OrderManager : MonoBehaviour
         if (orderConformations == 0)
         {
             timerManager.StartTimer();
-            OrderPlaceHolder.SetActive(false);
+            OrderPlaceHolder.GameObject().SetActive(false);
             RandomizeOrder();
             OrderImage.gameObject.SetActive(true);
             orderConformations++;
@@ -34,16 +44,26 @@ public class OrderManager : MonoBehaviour
         }
         else if(orderConformations < orderHolder.Count)
         {
-            
+            orderHolder.RemoveAt(currentOrderIndex);
+            RandomizeOrder();
+            orderConformations++;
+            return;
         }
         else
         {
-            
+            orderHolder.RemoveAt(currentOrderIndex);
+            OrderImage.gameObject.SetActive(false);
+            OrderPlaceHolder.text = "all orders completed, dont forget to clock out";
+            SpaceToConfirm.SetActive(false);
+            OrderPlaceHolder.GameObject().SetActive(true);
+            timerManager.PauseTimer();
         }
     }
 
     private void RandomizeOrder()
     {
-        currentOrder = orderHolder[Random.Range(0, orderHolder.Count)];
+        currentOrderIndex = Random.Range(0, orderHolder.Count);
+        currentOrder = orderHolder[currentOrderIndex];
+        OrderImage.sprite = currentOrder.orderImage;
     }
 }
